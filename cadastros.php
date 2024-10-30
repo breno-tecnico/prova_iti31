da160ce13b361e95a70ea465b0cbb2f04bb0a22f
breno-tecnico <guedesbreno458@gmail.com>
2024-10-30 08:12:02 -0300

    Adicionado style.css e página cadastros.php

--- cadastros.php
+++ cadastros.php
@@ -4,13 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css" >
 </head>
 <body>
-    <h1>BRENO GUEDES E HANIEL FILIPE</h1>
-
-    <a href="cadastros.php"><button>Cadastros</button></a>
-
-    <img src= "imagens/pizza.jpg">
+    <h1>CADASTROS</h1>
 </body>
 </html>
